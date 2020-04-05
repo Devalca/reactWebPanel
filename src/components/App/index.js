9072835037df5846da526e622edf3eeb09d1dd94
amatoxin404 <cyberregin@gmail.com>
2020-04-05 11:13:33 +0700

    last Commit

---
+++ src/components/App/index.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react'
+import './styles.css'
+import HomePage from '../HomePage'
+import Login from '../Login'
+import Register from '../Register'
+import Dashboard from '../Dashboard'
+import RsCalon from '../Register/calon'
+import RsUser from '../Register/user'
+import ChartVote from '../List/charVote'
+import ListData from '../List/listData'
+import UploadImage from '../Register/upload'
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
+import { CssBaseline, CircularProgress } from '@material-ui/core'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import firebase from '../firebase'
+
+const theme = createMuiTheme()
+
+export default function App() {
+
+	const [firebaseInitialized, setFirebaseInitialized] = useState(false)
+
+	useEffect(() => {
+		firebase.isInitialized().then(val => {
+			setFirebaseInitialized(val)
+		})
+	})
+
+
+	return firebaseInitialized !== false ? (
+		<MuiThemeProvider theme={theme}>
+			<CssBaseline />
+			<Router>
+				<Switch>
+					<Route exact path="/" component={HomePage} />
+					<Route exact path="/login" component={Login} />
+					<Route exact path="/register" component={Register} />
+					<Route exact path="/dashboard" component={Dashboard} />
+					<Route exact path="/calon" component={RsCalon} />
+					<Route exact path="/upload" component={UploadImage} />
+					<Route exact path="/user" component={RsUser} />
+					<Route exact path="/listdata" component={ListData} />
+					<Route exact path="/vote" component={ChartVote} />
+				</Switch>
+			</Router>
+		</MuiThemeProvider>
+	) : <div id="loader"><CircularProgress /></div>
+}
---
+++ src/components/firebase.js
@@ -0,0 +1,91 @@
+import app from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firebase-firestore'
+import 'firebase/storage'
+
+const config = {
+    apiKey: "AIzaSyBt4od9EVTpQErLWYRq8Z7dihbpyQFVVoc",
+    authDomain: "evote-3eb91.firebaseapp.com",
+    databaseURL: "https://evote-3eb91.firebaseio.com",
+    projectId: "evote-3eb91",
+    storageBucket: "evote-3eb91.appspot.com",
+    messagingSenderId: "101778515866",
+    appId: "1:101778515866:web:8b0c6cc411063259d13c23"
+};
+
+class Firebase {
+	constructor() {
+		app.initializeApp(config)
+		this.auth = app.auth()
+		this.db = app.firestore()
+		this.upload = app.storage()
+	}
+
+	getUpload() {
+		const storage = app.storage();
+		return storage
+	}
+
+	async login(email, password) {
+		await this.auth.signInWithEmailAndPassword(email, password)
+		if (!this.auth.currentUser) {
+		const status = await this.db.doc(`status/${this.auth.currentUser.uid}`).get()
+		if (!status) {
+			return status.get('status')
+		}
+		return null
+		} return null
+	}
+
+	logout() {
+		return this.auth.signOut()
+	}
+
+	async register(nama, email, password) {
+		await this.auth.createUserWithEmailAndPassword(email, password)
+		return this.auth.currentUser.updateProfile({
+			displayName: nama,
+		})
+	}
+
+	addCalon(nama, nim, prodi, angkatan, nourut, detail2, visi, misi, foto, hasil) {
+		if(!this.auth.currentUser) {
+			return alert('Not authorized')
+		}
+
+		return this.db.collection(`calon`).add({
+			nama, nim, prodi, angkatan, nourut, detail2, visi, misi, foto, hasil
+		})
+	}
+
+	addUser(nama, nim, prodi, email) {
+		if(!this.auth.currentUser) {
+			return alert('Not authorized')
+		}
+
+		return this.db.doc(`users/${this.auth.currentUser.uid}`).set({
+			nama, nim, prodi, email
+		})
+	}
+
+	isInitialized() {
+		return new Promise(resolve => {
+			this.auth.onAuthStateChanged(resolve)
+		})
+	}
+
+	getCurrentUsername() {
+		return this.auth.currentUser && this.auth.currentUser.displayName
+	}
+
+	async getCurrentAdmin() {
+		if(!this.auth.currentUser) {
+			return alert('Silahkan Masuk Sebagai Admin')
+		} else {
+		const status = await this.db.doc(`status/${this.auth.currentUser.uid}`).get()
+		return status.get('status')
+		}
+	}
+}
+
+export default new Firebase()
